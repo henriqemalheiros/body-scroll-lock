fac26bb60fceedeb8761a5df1f32beb5075b9be9
Henrique Malheiros <henriqemalheiros@gmail.com>
2021-10-13 22:05:39 -0300

    v7

--- examples/modal/main.js
+++ examples/modal/main.js
@@ -8,7 +8,7 @@ const scrollTargetElement = document.querySelector('.scrollTarget');
 
 window.onload = function () {
   const versionEl = document.getElementById('version');
-  versionEl.innerText = 'v6'
+  versionEl.innerText = 'v7'
 }
 
 disableBodyScrollButton.onclick = function() {
